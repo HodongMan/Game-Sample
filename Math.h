e76b7ab5467cf53148503ad144bef952f5958b5d
hodongman <jhd9206@gmail.com>
2019-01-01 23:35:10 +0900

    math library update

--- Math.h
+++ Math.h
@@ -3,6 +3,7 @@
 #include <cmath>
 #include <memory.h>
 #include <limits>
+#include "TypeDefine.h"
 
 namespace Math
 {
@@ -230,4 +231,734 @@ public:
 	float x;
 	float y;
 	float z;
+
+	Vector3() noexcept
+		: x( 0.0f )
+		, y( 0.0f )
+		, z( 0.0f )
+	{
+	
+	}
+
+	explicit Vector3( float inX, float inY, float inZ ) noexcept
+		: x( inX )
+		, y( inY )
+		, z( inZ )
+	{
+		
+	}
+
+	const float* GetAsFloatPtr() const noexcept
+	{
+		return reinterpret_cast<const float*>( &x );
+	}
+
+	void Set( float inX, float inY, float inZ ) noexcept
+	{
+		x = inX;
+		y = inY;
+		z = inZ;
+	}
+
+	friend Vector3 operator+( const Vector3& lhs, const Vector3& rhs ) noexcept
+	{
+		return Vector3( lhs.x + rhs.x, lhs.y + rhs.y, lhs.z + rhs.z );
+	}
+
+	friend Vector3 operator-( const Vector3& lhs, const Vector3& rhs ) noexcept
+	{
+		return Vector3( lhs.x - rhs.x, lhs.y - rhs.y, lhs.z - rhs.z );
+	}
+
+	friend Vector3 operator*( const Vector3& lhs, const Vector3& rhs ) noexcept
+	{
+		return Vector3( lhs.x * rhs.x, lhs.y * rhs.y, lhs.z * rhs.z );
+	}
+
+	friend Vector3 operator*( const Vector3& vector, float scalar ) noexcept
+	{
+		return Vector3( vector.x * scalar, vector.y * scalar, vector.z * scalar );
+	}
+
+	friend Vector3 operator*( float scalar, const Vector3& vector ) noexcept
+	{
+		return Vector3( vector.x * scalar, vector.y * scalar, vector.z * scalar );
+	}
+
+	Vector3& operator*=( float scalar ) noexcept
+	{
+		x *= scalar;
+		y *= scalar;
+		z *= scalar;
+
+		return *this;
+	}
+
+	Vector3& operator+=( const Vector3& rhs ) noexcept
+	{
+		x += rhs.x;
+		y += rhs.y;
+		z += rhs.z;
+
+		return *this;
+	}
+
+	Vector3& operator-=( const Vector3& rhs ) noexcept
+	{
+		x -= rhs.x;
+		y -= rhs.y;
+		z -= rhs.z;
+
+		return *this;
+	}
+
+	float LengthSq() const noexcept
+	{
+		return ( ( x * x ) + ( y * y ) + ( z * z ) );
+	}
+
+	float Length() const noexcept
+	{
+		return ( Math::Sqrt( LengthSq() ) );
+	}
+
+	void Normalize() noexcept
+	{
+		float length = Length();
+		x /= length;
+		y /= length;
+		z /= length;
+	}
+
+	static Vector3 Normalize( const Vector3& vector ) noexcept
+	{
+		Vector3 temp = vector;
+		temp.Normalize();
+		return temp;
+	}
+
+	static float Dot( const Vector3& lhs, const Vector3& rhs ) noexcept
+	{
+		return ( ( lhs.x * rhs.x ) + ( lhs.y * rhs.y ) + ( lhs.z * rhs.z ) );
+	}
+
+	static Vector3 Cross( const Vector3& lhs, const Vector3& rhs ) noexcept
+	{
+		Vector3 temp;
+		temp.x = ( lhs.y * rhs.z ) - ( lhs.z * rhs.y );
+		temp.y = ( lhs.z * rhs.x ) - ( lhs.x * rhs.z );
+		temp.z = ( lhs.x * rhs.y ) - ( lhs.y * rhs.x );
+
+		return temp;
+	}
+
+	static Vector3 Lerp( const Vector3& lhs, const Vector3& rhs, float f ) noexcept
+	{
+		return Vector3( lhs + f * ( rhs - lhs ) );
+	}
+	
+	static Vector3 Reflect(const Vector3& v, const Vector3& n) noexcept
+	{
+		return v - 2.0f * Vector3::Dot( v, n ) * n;
+	}
+
+	static Vector3 Transform( const Vector3& vector, const class Matrix4& matrix, float w = 1.0f ) noexcept;
+	
+	static Vector3 TransformWithPerspDiv( const Vector3& vector, const class Matrix4& matrix, float w = 1.0f ) noexcept;
+
+	static Vector3 Transform( const Vector3& vector, const class Quaternion& q ) noexcept;
+
+	static const Vector3 Zero;
+	static const Vector3 UnitX;
+	static const Vector3 UnitY;
+	static const Vector3 UnitZ;
+	static const Vector3 NegUnitX;
+	static const Vector3 NegUnitY;
+	static const Vector3 NegUnitZ;
+	static const Vector3 Infinity;
+	static const Vector3 NegInfinity;
 };
+
+class Matrix3
+{
+public:
+	float matrix[MathType::Matrix3Size][MathType::Matrix3Size];
+
+	Matrix3() noexcept
+	{
+		*this = Matrix3::Identity;
+	}
+
+	explicit Matrix3( float inMat[MathType::Matrix3Size][MathType::Matrix3Size] ) noexcept
+	{
+		memcpy( matrix, inMat, MathType::Matrix3TotalSize * sizeof( float ) );
+	}
+
+	const float* GetAsFloatPtr() const noexcept
+	{
+		return reinterpret_cast<const float*>( &matrix[0][0] );
+	}
+
+	friend Matrix3 operator*( const Matrix3& lhs, const Matrix3& rhs ) noexcept
+	{
+		Matrix3 retVal;
+
+		retVal.matrix[0][0] = 
+			lhs.matrix[0][0] * rhs.matrix[0][0] +
+			lhs.matrix[0][1] * rhs.matrix[1][0] +
+			lhs.matrix[0][2] * rhs.matrix[2][0];
+
+		retVal.matrix[0][1] = 
+			lhs.matrix[0][0] * rhs.matrix[0][1] +
+			lhs.matrix[0][1] * rhs.matrix[1][1] +
+			lhs.matrix[0][2] * rhs.matrix[2][1];
+
+		retVal.matrix[0][2] = 
+			lhs.matrix[0][0] * rhs.matrix[0][2] +
+			lhs.matrix[0][1] * rhs.matrix[1][2] +
+			lhs.matrix[0][2] * rhs.matrix[2][2];
+		
+		retVal.matrix[1][0] = 
+			lhs.matrix[1][0] * rhs.matrix[0][0] +
+			lhs.matrix[1][1] * rhs.matrix[1][0] +
+			lhs.matrix[1][2] * rhs.matrix[2][0];
+
+		retVal.matrix[1][1] = 
+			lhs.matrix[1][0] * rhs.matrix[0][1] +
+			lhs.matrix[1][1] * rhs.matrix[1][1] +
+			lhs.matrix[1][2] * rhs.matrix[2][1];
+
+		retVal.matrix[1][2] = 
+			lhs.matrix[1][0] * rhs.matrix[0][2] +
+			lhs.matrix[1][1] * rhs.matrix[1][2] +
+			lhs.matrix[1][2] * rhs.matrix[2][2];
+		
+		retVal.matrix[2][0] = 
+			lhs.matrix[2][0] * rhs.matrix[0][0] +
+			lhs.matrix[2][1] * rhs.matrix[1][0] +
+			lhs.matrix[2][2] * rhs.matrix[2][0];
+
+		retVal.matrix[2][1] =
+			lhs.matrix[2][0] * rhs.matrix[0][1] +
+			lhs.matrix[2][1] * rhs.matrix[1][1] +
+			lhs.matrix[2][2] * rhs.matrix[2][1];
+
+		retVal.matrix[2][2] = 
+			lhs.matrix[2][0] * rhs.matrix[0][2] +
+			lhs.matrix[2][1] * rhs.matrix[1][2] +
+			lhs.matrix[2][2] * rhs.matrix[2][2];
+
+		return retVal;
+	}
+
+
+	Matrix3& operator*=( const Matrix3& rhs ) noexcept
+	{
+		*this = *this * rhs;
+
+		return *this;
+	}
+
+	static Matrix3 CreateScale( float xScale, float yScale ) noexcept
+	{
+		float temp[MathType::Matrix3Size][MathType::Matrix3Size] =
+		{
+			{ xScale, 0.0f, 0.0f },
+			{ 0.0f, yScale, 0.0f },
+			{ 0.0f, 0.0f, 1.0f },
+		};
+
+		return Matrix3( temp );
+	}
+	
+	static Matrix3 CreateScale( const Vector2& scaleVector ) noexcept
+	{
+		return CreateScale( scaleVector.x, scaleVector.y );
+	}
+
+	static Matrix3 CreateScale( float scale ) noexcept
+	{
+		return CreateScale( scale, scale );
+	}
+
+	static Matrix3 CreateRotation( float theta ) noexcept
+	{
+		float temp[MathType::Matrix3Size][MathType::Matrix3Size] =
+		{
+			{ Math::Cos( theta ), Math::Sin( theta ), 0.0f },
+			{ -Math::Sin( theta ), Math::Cos( theta ), 0.0f },
+			{ 0.0f, 0.0f, 1.0f },
+		};
+		return Matrix3( temp );
+	}
+
+	static Matrix3 CreateTranslation( const Vector2& trans ) noexcept
+	{
+		float temp[MathType::Matrix3Size][MathType::Matrix3Size] =
+		{
+			{ 1.0f, 0.0f, 0.0f },
+			{ 0.0f, 1.0f, 0.0f },
+			{ trans.x, trans.y, 1.0f },
+		};
+		return Matrix3( temp );
+	}
+
+	static const Matrix3 Identity;
+};
+
+class Matrix4
+{
+public:
+	float mat[4][4];
+
+	Matrix4()
+	{
+		*this = Matrix4::Identity;
+	}
+
+	explicit Matrix4(float inMat[4][4])
+	{
+		memcpy(mat, inMat, 16 * sizeof(float));
+	}
+
+	const float* GetAsFloatPtr() const
+	{
+		return reinterpret_cast<const float*>(&mat[0][0]);
+	}
+
+	friend Matrix4 operator*(const Matrix4& a, const Matrix4& b)
+	{
+		Matrix4 retVal;
+		// row 0
+		retVal.mat[0][0] = 
+			a.mat[0][0] * b.mat[0][0] + 
+			a.mat[0][1] * b.mat[1][0] + 
+			a.mat[0][2] * b.mat[2][0] +
+			a.mat[0][3] * b.mat[3][0];
+
+		retVal.mat[0][1] = 
+			a.mat[0][0] * b.mat[0][1] + 
+			a.mat[0][1] * b.mat[1][1] + 
+			a.mat[0][2] * b.mat[2][1] + 
+			a.mat[0][3] * b.mat[3][1];
+
+		retVal.mat[0][2] = 
+			a.mat[0][0] * b.mat[0][2] + 
+			a.mat[0][1] * b.mat[1][2] + 
+			a.mat[0][2] * b.mat[2][2] + 
+			a.mat[0][3] * b.mat[3][2];
+		
+		retVal.mat[0][3] = 
+			a.mat[0][0] * b.mat[0][3] + 
+			a.mat[0][1] * b.mat[1][3] + 
+			a.mat[0][2] * b.mat[2][3] + 
+			a.mat[0][3] * b.mat[3][3];
+
+		// row 1
+		retVal.mat[1][0] = 
+			a.mat[1][0] * b.mat[0][0] + 
+			a.mat[1][1] * b.mat[1][0] + 
+			a.mat[1][2] * b.mat[2][0] + 
+			a.mat[1][3] * b.mat[3][0];
+
+		retVal.mat[1][1] = 
+			a.mat[1][0] * b.mat[0][1] + 
+			a.mat[1][1] * b.mat[1][1] + 
+			a.mat[1][2] * b.mat[2][1] + 
+			a.mat[1][3] * b.mat[3][1];
+
+		retVal.mat[1][2] = 
+			a.mat[1][0] * b.mat[0][2] + 
+			a.mat[1][1] * b.mat[1][2] + 
+			a.mat[1][2] * b.mat[2][2] + 
+			a.mat[1][3] * b.mat[3][2];
+
+		retVal.mat[1][3] = 
+			a.mat[1][0] * b.mat[0][3] +
+			a.mat[1][1] * b.mat[1][3] +
+			a.mat[1][2] * b.mat[2][3] +
+			a.mat[1][3] * b.mat[3][3];
+
+		// row 2
+		retVal.mat[2][0] = 
+			a.mat[2][0] * b.mat[0][0] +
+			a.mat[2][1] * b.mat[1][0] +
+			a.mat[2][2] * b.mat[2][0] +
+			a.mat[2][3] * b.mat[3][0];
+
+		retVal.mat[2][1] = 
+			a.mat[2][0] * b.mat[0][1] + 
+			a.mat[2][1] * b.mat[1][1] + 
+			a.mat[2][2] * b.mat[2][1] + 
+			a.mat[2][3] * b.mat[3][1];
+
+		retVal.mat[2][2] = 
+			a.mat[2][0] * b.mat[0][2] +
+			a.mat[2][1] * b.mat[1][2] + 
+			a.mat[2][2] * b.mat[2][2] + 
+			a.mat[2][3] * b.mat[3][2];
+
+		retVal.mat[2][3] = 
+			a.mat[2][0] * b.mat[0][3] + 
+			a.mat[2][1] * b.mat[1][3] + 
+			a.mat[2][2] * b.mat[2][3] + 
+			a.mat[2][3] * b.mat[3][3];
+
+		// row 3
+		retVal.mat[3][0] = 
+			a.mat[3][0] * b.mat[0][0] + 
+			a.mat[3][1] * b.mat[1][0] + 
+			a.mat[3][2] * b.mat[2][0] + 
+			a.mat[3][3] * b.mat[3][0];
+
+		retVal.mat[3][1] = 
+			a.mat[3][0] * b.mat[0][1] + 
+			a.mat[3][1] * b.mat[1][1] + 
+			a.mat[3][2] * b.mat[2][1] + 
+			a.mat[3][3] * b.mat[3][1];
+
+		retVal.mat[3][2] = 
+			a.mat[3][0] * b.mat[0][2] +
+			a.mat[3][1] * b.mat[1][2] +
+			a.mat[3][2] * b.mat[2][2] +
+			a.mat[3][3] * b.mat[3][2];
+
+		retVal.mat[3][3] = 
+			a.mat[3][0] * b.mat[0][3] +
+			a.mat[3][1] * b.mat[1][3] +
+			a.mat[3][2] * b.mat[2][3] +
+			a.mat[3][3] * b.mat[3][3];
+		
+		return retVal;
+	}
+
+	Matrix4& operator*=(const Matrix4& right)
+	{
+		*this = *this * right;
+		return *this;
+	}
+
+	void Invert();
+
+	Vector3 GetTranslation() const
+	{
+		return Vector3(mat[3][0], mat[3][1], mat[3][2]);
+	}
+	
+	Vector3 GetXAxis() const
+	{
+		return Vector3::Normalize(Vector3(mat[0][0], mat[0][1], mat[0][2]));
+	}
+
+	Vector3 GetYAxis() const
+	{
+		return Vector3::Normalize(Vector3(mat[1][0], mat[1][1], mat[1][2]));
+	}
+
+	Vector3 GetZAxis() const
+	{
+		return Vector3::Normalize(Vector3(mat[2][0], mat[2][1], mat[2][2]));
+	}
+
+	Vector3 GetScale() const
+	{
+		Vector3 retVal;
+		retVal.x = Vector3(mat[0][0], mat[0][1], mat[0][2]).Length();
+		retVal.y = Vector3(mat[1][0], mat[1][1], mat[1][2]).Length();
+		retVal.z = Vector3(mat[2][0], mat[2][1], mat[2][2]).Length();
+		return retVal;
+	}
+
+	static Matrix4 CreateScale(float xScale, float yScale, float zScale)
+	{
+		float temp[4][4] =
+		{
+			{ xScale, 0.0f, 0.0f, 0.0f },
+			{ 0.0f, yScale, 0.0f, 0.0f },
+			{ 0.0f, 0.0f, zScale, 0.0f },
+			{ 0.0f, 0.0f, 0.0f, 1.0f }
+		};
+		return Matrix4(temp);
+	}
+
+	static Matrix4 CreateScale(const Vector3& scaleVector)
+	{
+		return CreateScale(scaleVector.x, scaleVector.y, scaleVector.z);
+	}
+
+	static Matrix4 CreateScale(float scale)
+	{
+		return CreateScale(scale, scale, scale);
+	}
+
+	static Matrix4 CreateRotationX(float theta)
+	{
+		float temp[4][4] =
+		{
+			{ 1.0f, 0.0f, 0.0f , 0.0f },
+			{ 0.0f, Math::Cos(theta), Math::Sin(theta), 0.0f },
+			{ 0.0f, -Math::Sin(theta), Math::Cos(theta), 0.0f },
+			{ 0.0f, 0.0f, 0.0f, 1.0f },
+		};
+		return Matrix4(temp);
+	}
+
+	static Matrix4 CreateRotationY(float theta)
+	{
+		float temp[4][4] =
+		{
+			{ Math::Cos(theta), 0.0f, -Math::Sin(theta), 0.0f },
+			{ 0.0f, 1.0f, 0.0f, 0.0f },
+			{ Math::Sin(theta), 0.0f, Math::Cos(theta), 0.0f },
+			{ 0.0f, 0.0f, 0.0f, 1.0f },
+		};
+		return Matrix4(temp);
+	}
+
+	static Matrix4 CreateRotationZ(float theta)
+	{
+		float temp[4][4] =
+		{
+			{ Math::Cos(theta), Math::Sin(theta), 0.0f, 0.0f },
+			{ -Math::Sin(theta), Math::Cos(theta), 0.0f, 0.0f },
+			{ 0.0f, 0.0f, 1.0f, 0.0f },
+			{ 0.0f, 0.0f, 0.0f, 1.0f },
+		};
+		return Matrix4(temp);
+	}
+
+	static Matrix4 CreateFromQuaternion(const class Quaternion& q);
+
+	static Matrix4 CreateTranslation(const Vector3& trans)
+	{
+		float temp[4][4] =
+		{
+			{ 1.0f, 0.0f, 0.0f, 0.0f },
+			{ 0.0f, 1.0f, 0.0f, 0.0f },
+			{ 0.0f, 0.0f, 1.0f, 0.0f },
+			{ trans.x, trans.y, trans.z, 1.0f }
+		};
+		return Matrix4(temp);
+	}
+
+	static Matrix4 CreateLookAt(const Vector3& eye, const Vector3& target, const Vector3& up)
+	{
+		Vector3 zaxis = Vector3::Normalize(target - eye);
+		Vector3 xaxis = Vector3::Normalize(Vector3::Cross(up, zaxis));
+		Vector3 yaxis = Vector3::Normalize(Vector3::Cross(zaxis, xaxis));
+		Vector3 trans;
+		trans.x = -Vector3::Dot(xaxis, eye);
+		trans.y = -Vector3::Dot(yaxis, eye);
+		trans.z = -Vector3::Dot(zaxis, eye);
+
+		float temp[4][4] =
+		{
+			{ xaxis.x, yaxis.x, zaxis.x, 0.0f },
+			{ xaxis.y, yaxis.y, zaxis.y, 0.0f },
+			{ xaxis.z, yaxis.z, zaxis.z, 0.0f },
+			{ trans.x, trans.y, trans.z, 1.0f }
+		};
+		return Matrix4(temp);
+	}
+
+	static Matrix4 CreateOrtho(float width, float height, float near, float far)
+	{
+		float temp[4][4] =
+		{
+			{ 2.0f / width, 0.0f, 0.0f, 0.0f },
+			{ 0.0f, 2.0f / height, 0.0f, 0.0f },
+			{ 0.0f, 0.0f, 1.0f / (far - near), 0.0f },
+			{ 0.0f, 0.0f, near / (near - far), 1.0f }
+		};
+		return Matrix4(temp);
+	}
+
+	static Matrix4 CreatePerspectiveFOV(float fovY, float width, float height, float near, float far)
+	{
+		float yScale = Math::Cot(fovY / 2.0f);
+		float xScale = yScale * height / width;
+		float temp[4][4] =
+		{
+			{ xScale, 0.0f, 0.0f, 0.0f },
+			{ 0.0f, yScale, 0.0f, 0.0f },
+			{ 0.0f, 0.0f, far / (far - near), 1.0f },
+			{ 0.0f, 0.0f, -near * far / (far - near), 0.0f }
+		};
+		return Matrix4(temp);
+	}
+
+	static Matrix4 CreateSimpleViewProj(float width, float height)
+	{
+		float temp[4][4] =
+		{
+			{ 2.0f/width, 0.0f, 0.0f, 0.0f },
+			{ 0.0f, 2.0f/height, 0.0f, 0.0f },
+			{ 0.0f, 0.0f, 1.0f, 0.0f },
+			{ 0.0f, 0.0f, 1.0f, 1.0f }
+		};
+		return Matrix4(temp);
+	}
+	
+	static const Matrix4 Identity;
+};
+
+class Quaternion
+{
+public:
+	float x;
+	float y;
+	float z;
+	float w;
+
+	Quaternion() noexcept
+	{
+		*this = Quaternion::Identity;
+	}
+
+	explicit Quaternion( float inX, float inY, float inZ, float inW ) noexcept
+	{
+		Set( inX, inY, inZ, inW );
+	}
+
+	explicit Quaternion( const Vector3& axis, float angle ) noexcept
+	{
+		float scalar = Math::Sin( angle / 2.0f );
+		x = axis.x * scalar;
+		y = axis.y * scalar;
+		z = axis.z * scalar;
+		w = Math::Cos( angle / 2.0f );
+	}
+
+	void Set( float inX, float inY, float inZ, float inW ) noexcept
+	{
+		x = inX;
+		y = inY;
+		z = inZ;
+		w = inW;
+	}
+
+	void Conjugate() noexcept
+	{
+		x *= -1.0f;
+		y *= -1.0f;
+		z *= -1.0f;
+	}
+
+	float LengthSq() const noexcept
+	{
+		return ( ( x * x ) + ( y * y ) + ( z * z ) + ( w * w ) );
+	}
+
+	float Length() const noexcept
+	{
+		return Math::Sqrt( LengthSq() );
+	}
+
+	void Normalize() noexcept
+	{
+		float length = Length();
+		x /= length;
+		y /= length;
+		z /= length;
+		w /= length;
+	}
+
+	static Quaternion Normalize( const Quaternion& quaternion) noexcept
+	{
+		Quaternion retVal = quaternion;
+		retVal.Normalize();
+		
+		return retVal;
+	}
+
+	static Quaternion Lerp( const Quaternion& lhs, const Quaternion& rhs, float f ) noexcept
+	{
+		Quaternion retVal;
+		retVal.x = Math::Lerp( lhs.x, rhs.x, f );
+		retVal.y = Math::Lerp( lhs.y, rhs.y, f );
+		retVal.z = Math::Lerp( lhs.z, rhs.z, f );
+		retVal.w = Math::Lerp( lhs.w, rhs.w, f );
+		
+		retVal.Normalize();
+		
+		return retVal;
+	}
+
+	static float Dot( const Quaternion& lhs, const Quaternion& rhs ) noexcept
+	{
+		return lhs.x * rhs.x + lhs.y * rhs.y + lhs.z * rhs.z + lhs.w * rhs.w;
+	}
+
+	static Quaternion Slerp(const Quaternion& lhs, const Quaternion& rhs, float f) noexcept
+	{
+		float rawCosm = Quaternion::Dot( lhs, rhs );
+
+		float cosom = -rawCosm;
+		if ( 0.0f <= rawCosm )
+		{
+			cosom = rawCosm;
+		}
+
+		float scale0, scale1;
+
+		if ( cosom < 0.9999f )
+		{
+			const float omega = Math::Acos( cosom );
+			const float invSin = 1.f / Math::Sin( omega );
+			scale0 = Math::Sin( ( 1.f - f ) * omega ) * invSin;
+			scale1 = Math::Sin( f * omega ) * invSin;
+		}
+		else
+		{
+			scale0 = 1.0f - f;
+			scale1 = f;
+		}
+
+		if ( rawCosm < 0.0f )
+		{
+			scale1 = -scale1;
+		}
+
+		Quaternion retVal;
+		retVal.x = scale0 * lhs.x + scale1 * rhs.x;
+		retVal.y = scale0 * lhs .y + scale1 * rhs.y;
+		retVal.z = scale0 * lhs.z + scale1 * rhs.z;
+		retVal.w = scale0 * lhs.w + scale1 * rhs.w;
+		retVal.Normalize();
+		
+		return retVal;
+	}
+
+	static Quaternion Concatenate( const Quaternion& lhs, const Quaternion& rhs ) noexcept
+	{
+		Quaternion retVal;
+
+		Vector3 qv( lhs.x, lhs.y, lhs.z );
+		Vector3 pv( rhs.x, rhs.y, rhs.z );
+		Vector3 newVec = lhs.w * qv + rhs.w * pv + Vector3::Cross( pv, qv );
+		retVal.x = newVec.x;
+		retVal.y = newVec.y;
+		retVal.z = newVec.z;
+
+		retVal.w = lhs.w * rhs.w - Vector3::Dot( pv, qv );
+
+		return retVal;
+	}
+
+	static const Quaternion Identity;
+
+};
+
+namespace Color
+{
+	static const Vector3 Black( 0.0f, 0.0f, 0.0f );
+	static const Vector3 White( 1.0f, 1.0f, 1.0f );
+	static const Vector3 Red( 1.0f, 0.0f, 0.0f );
+	static const Vector3 Green( 0.0f, 1.0f, 0.0f );
+	static const Vector3 Blue( 0.0f, 0.0f, 1.0f );
+	static const Vector3 Yellow( 1.0f, 1.0f, 0.0f );
+	static const Vector3 LightYellow( 1.0f, 1.0f, 0.88f );
+	static const Vector3 LightBlue( 0.68f, 0.85f, 0.9f );
+	static const Vector3 LightPink( 1.0f, 0.71f, 0.76f );
+	static const Vector3 LightGreen( 0.56f, 0.93f, 0.56f );
+}
--- TypeDefine.h
+++ TypeDefine.h
@@ -1,4 +1,12 @@
 #pragma once
 
+////////////////////Math Types
+
+enum MathType : int
+{
+	Matrix3Size = 3,
+	Matrix3TotalSize = Matrix3Size * Matrix3Size,
+};
+
 const int thickness = 15;
 const float paddleH = 100.0f;
